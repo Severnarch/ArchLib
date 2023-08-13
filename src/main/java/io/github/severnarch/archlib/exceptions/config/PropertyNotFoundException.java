fa25525f1a035f6555ee951e8a197d2cadae2cf6
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2023-08-13 15:26:34 +0100

    Warning removals

--- src/main/java/io/github/severnarch/archlib/exceptions/config/PropertyNotFoundException.java
+++ src/main/java/io/github/severnarch/archlib/exceptions/config/PropertyNotFoundException.java
@@ -1,7 +1,5 @@
 package io.github.severnarch.archlib.exceptions.config;
 
-import java.io.FileNotFoundException;
-
 public class PropertyNotFoundException extends Throwable {
     public PropertyNotFoundException(String propertyName, String configurationName) {
         super("Property of name '"+propertyName+"' could not be found in configuration '"+configurationName+"'.");
